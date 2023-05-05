cd94fc7f2efc0849a276c53d4e56862777717270
thomasgrogan <thomasmgrogan1@gmail.com>
2023-05-05 10:17:49 +0100

    Navbar from mui created

--- country-app/src/components/App/App.js
+++ country-app/src/components/App/App.js
@@ -1,13 +1,13 @@
 // In the app we will display the navigation bar, the search bar, the dropdown menu, and the country cards.
 
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "../NavBar/navBar";
 
 function App() {
   return (
     <div className="App">
-      
-
+      <NavBar />
     </div>
   );
 }
--- country-app/src/components/NavBar/navBar.js
+++ country-app/src/components/NavBar/navBar.js
@@ -1,2 +1,25 @@
-// It will display the title 
+// It will display the title
 
+// We need to use MUI to import a navbar feature that will display a basic navbar with the title of the app
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Explore the World 🌎
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
